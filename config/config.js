edc1503e24f74fac8d2ca301c3ce547a95b77909
TABUE Romeo <tabueromeo@gmail.com>
2021-04-30 12:09:59 +0100

    ajout de la bd

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    CURRENT_BD_PATH:"mongodb://localhost/tbrannonces",
+    "secret" : "romeotabuecript",
+    //CURRENT_IMGS_PATH: "/frontclient/build/images/"
+    //CURRENT_IMGS_PATH: "/frontclient/public/images/"
+    
+}
--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const path = require('path')
+const app = express();
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+
+
+mongoose.connect(config.CURRENT_BD_PATH).then(() => {
+  console.log('Connected to mongoDB')
+}).catch(e => {
+  console.log('Error while DB connecting');
+  console.log(e);
+});
+app.use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.send(" Real time POS3 web app running."))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  //Définition des CORS
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
