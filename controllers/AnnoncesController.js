22a59516c88bec0c5ca61ff61df4bbfa773c3594
tabueromeo <68849253+tabueromeo@users.noreply.github.com>
2021-12-30 15:56:39 +0100

    modif read to readannonce

--- controllers/AnnoncesController.js
+++ controllers/AnnoncesController.js
@@ -3,7 +3,7 @@ const annonce = require('../model/Annonces');
 module.exports = function (app) {
     console.log("entre_____________________________controleurAnnonce")
     app.post('/addannonce',annonce.addannonce);
-    app.get('/read',annonce.showAllAnnonce);
+    app.get('/readannonce',annonce.showAllAnnonce);
     app.get('/one',annonce.showOneAnnonce)
   //  app.get('/show-by-criteria',logement.showByCriteria)
     app.post('/update',annonce.updateAnnonce)
