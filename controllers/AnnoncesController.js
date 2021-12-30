06d2e3d2537b8021e68b33c26448fd9be70349f1
tabueromeo <68849253+tabueromeo@users.noreply.github.com>
2021-12-30 15:46:30 +0100

    mis à niveau nom de function dans annoncesControll

--- controllers/AnnoncesController.js
+++ controllers/AnnoncesController.js
@@ -7,5 +7,5 @@ module.exports = function (app) {
     app.get('/one',annonce.showOneAnnonce)
   //  app.get('/show-by-criteria',logement.showByCriteria)
     app.post('/update',annonce.updateAnnonce)
-    app.post('/deletelogement',annonce.deleteannonceAnnonce)
+    app.post('/deletelogement',annonce.deleteannonce)
 }
